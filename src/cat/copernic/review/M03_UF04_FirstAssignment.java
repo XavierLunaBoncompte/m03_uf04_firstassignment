2c582d6c9ee0092abf29707c913712ad46638e98
xaviLuna <luna.boncompte.xavier@alumnat.copernic.cat>
2020-10-13 22:20:42 +0200

    Clase del Triangulo acabada. Se calcula la área y perímetro correctamente en la clase principal

--- src/cat/copernic/review/M03_UF04_FirstAssignment.java
+++ src/cat/copernic/review/M03_UF04_FirstAssignment.java
@@ -16,10 +16,40 @@ public class M03_UF04_FirstAssignment {
         // Prueba to código aquí
         CuentaCorriente cc1 = new CuentaCorriente("Pedro", 1_000.0);
         System.out.println(cc1);
-        cc1.ingresa(123.09);
+        try {
+            cc1.ingresa(123.09);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         System.out.println(cc1);
-        cc1.abona(98.87);
+        try {
+            cc1.abona(98.87);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println(cc1);
+
+        Cuadrado c1;
+        System.out.println("CUADRADO\n");
+        try {
+            c1 = new Cuadrado(9);
+            System.out.println("Area; " + c1.area());
+            System.out.println("Perimetro; " + c1.perimetro()+ "\n");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        Triangulo t1;
+        System.out.println("TRIANGULO\n");
+        
+        try {
+            t1 = new Triangulo(6, 3);
+            System.out.println("Area; " + t1.area());
+            System.out.println("Perimetro; " + t1.perimetro());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
     
 }
--- src/cat/copernic/review/Triangulo.java
+++ src/cat/copernic/review/Triangulo.java
@@ -10,25 +10,65 @@ package cat.copernic.review;
  * @author pep
  */
 public class Triangulo {
-    
+
     // propiedades
     private double base;
     private double altura;
-    
+
+    public Triangulo(double base, double altura) throws Exception {
+        if (base <= 0) {
+            throw new Exception("La base tiene que ser positiva");
+        }
+        this.base = base;
+
+        if (altura <= 0) {
+            throw new Exception("La altura tiene que ser positiva");
+        }
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangulo{" + "base=" + base + ", altura=" + altura + '}';
+    }
+
     /* Calcula el área de este triangulo */
     public double area() {
-        return 0.0;
+        return (base * altura) / 2;
     }
-    
+
     /* Calcula el perímetro de este triangulo conocida su base y altura */
     public double perimetro() {
-        // Math.sqrt(345.9);
-        // Math.pow(n, 2.0);
-        return 0.0;
+        double lado;
+
+        lado = calculaHipotenusa();
+        return lado + lado + base;
     }
-    
-//    private double calcHipotenusa() {
-//        
-//    }
-    
+
+    private double calculaHipotenusa() {
+        double h;
+        double costatBase;
+
+        costatBase = base / 2;
+        h = Math.sqrt(Math.pow(altura, 2.0) + Math.pow(costatBase, 2.0));
+        System.out.println("Hipotenusa; " + h);
+        return h;
+    }
+
 }
